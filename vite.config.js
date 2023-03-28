02bbc82c961746f847c14965037294ed866a3671
AntonioRM <ca.riveros.m16@gmail.com>
2023-03-27 19:01:07 -0600

    reupdate base vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://github.com/antonio-rm16/06-Tabs/",
+  base:'https://antonio-rm16.github.io/06-Tabs/',
 })
